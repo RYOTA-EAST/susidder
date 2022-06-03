e21e10e294566999221f3ca334efc8f47a76d7b1
Ryota Higashi <ryo19870130@outlook.jp>
2022-06-03 21:24:26 +0900

    FormView開発

--- twitter/views.py
+++ twitter/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import ListView, FormView, CreateView, UpdateView, View
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 from .models import Tweet
+from .forms import TweetFrom
 
 class IndexView(ListView):
 
@@ -9,3 +12,25 @@ class IndexView(ListView):
   queryset = Tweet.objects.filter(is_sent=False, is_deleted=False)
 
 index = IndexView.as_view()
+
+class FormViewBase(FormView):
+  model = Tweet
+  template_name = 'twitter/form.html'
+  success_url = reverse_lazy('twitter:index')
+  message = ''
+
+  def form_valid(self, form):
+    tweet = form.save(commit=False)
+    tweet.user_id = self.request.user.idx
+    tweet.save()
+    messages.success(self.request, '成功しました')
+
+class TweetCreateView(FormViewBase):
+  message = 'ツイートを予約しました'
+
+create = TweetCreateView.as_view()
+
+class TweetUpdateView(FormViewBase, UpdateView):
+  message = '予約ツイート内容を変更しました'
+
+update = TweetUpdateView.as_view()
