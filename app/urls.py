6bf268802a4612cc1fb99346be452571288586fe
Ryota Higashi <ryo19870130@outlook.jp>
2022-06-03 21:12:20 +0900

    indexView開発

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,8 @@
-from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('twitter.urls')),
+    path('', include('twitter.urls')),
     # path('', include('account.urls'))
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView, FormView, CreateView, UpdateView, View
 
-# Create your views here.
+from .models import Tweet
+
+class IndexView(ListView):
+
+  template_name = 'twitter/index.html'
+  context_object_name = 'tweets'
+  queryset = Tweet.objects.filter(is_sent=False, is_deleted=False)
+
+index = IndexView.as_view()
